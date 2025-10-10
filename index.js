28a4ee5d3b59118176dff5e0057c5216d64cb508
ArmanHossain04 <armanhossain0477@gmail.com>
2025-10-10 20:24:09 +0600

    feat: created post api on the server

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-
-
-
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mk63pzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mk63pzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,6 +28,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("parcelDB"); //database name
+    const parcelCollection = db.collection("parcels"); //collection
+
+    app.get("/parcels"),
+      async (req, res) => {
+        const parcels = await parcelCollection.find().toArray();
+        res.send(parcels);
+      };
+
+    // POST : create a new parcel
+    app.post("/parcels", async (req, res) => {
+      try {
+        const newParcel = req.body;
+        const result = await parcelCollection.insertOne(newParcel);
+        res.status(201).send(result);
+      } catch (error) {
+        console.error("Error inserting parcel:", error);
+        res.status(500).send({ message: "Failed to create parcel" });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
